229e3ce88596cf450b732bf5969b3c128072b0bf
shiron <shiron4710.dev@gmail.com>
2024-01-25 09:23:02 +0900

    feat: set iface when scan

--- server/server/net_tools/scan.py
+++ server/server/net_tools/scan.py
@@ -2,19 +2,16 @@ from scapy.all import ARP, Ether, srp
 import socket
 
 
-def scan(ip):
-    # ARPパケットを作成して送信
+def scan(ip, iface=None):
     arp_request = ARP(pdst=ip)
-    ether_frame = Ether(dst="ff:ff:ff:ff:ff:ff")  # ブロードキャストアドレスに送信
+    ether_frame = Ether(dst="ff:ff:ff:ff:ff:ff")
     packet = ether_frame / arp_request
 
-    result = srp(packet, timeout=3, verbose=0)[0]
+    result = srp(packet, iface=iface, timeout=3, verbose=0)[0]
 
-    # 取得したデバイスの情報を格納
     devices = []
     for sent, received in result:
         try:
-            # ホスト名の取得
             hostname = received.hostname
         except:
             hostname = "N/A"
@@ -23,12 +20,12 @@ def scan(ip):
     return devices
 
 
-def get_lan_devices(target_ip=None):
+def get_lan_devices(target_ip=None, iface=None):
     if target_ip is None:
         my_ip = socket.gethostbyname(socket.gethostname())
         gateway = ".".join(my_ip.split(".")[0:-1])
         target_ip = f"{gateway}/24"
 
-    devices = scan(target_ip)
+    devices = scan(target_ip, iface=iface)
 
     return devices
